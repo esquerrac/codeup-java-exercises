dd6edb75f4c76fb6fdb0f9edb9a0175f4c9681df
chrisesquerra <esquerra>
2018-09-17 14:49:23 -0500

    Continued exercise completion

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,17 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 39;
+        System.out.println(myFavoriteNumber);
+        String myString= "A series of words";
+        System.out.println(myString);
+        long myNumber = 123;
+        System.out.println(myNumber);
+        int x= 10;
+        int y=2;
+        x/=y;
+        System.out.println(x);
+        y-=x;
+        System.out.println(y);
     }
 }
