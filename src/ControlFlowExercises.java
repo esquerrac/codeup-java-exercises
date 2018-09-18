862ac51ff66d4ca55c58a1d8d4dcb7cc2d72b4ca
chrisesquerra <esquerra>
2018-09-18 14:46:13 -0500

    Continued exercise completion

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,10 @@
 public class ControlFlowExercises {
     public static void main(String[] args){
-        int i = 5;
-        while (i<=15){
+        int i = 0;
+        do{
+            i+=2;
             System.out.println(i);
-            i++;
-        }
+
+        }while (i<100);
     }
 }
