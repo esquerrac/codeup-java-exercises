d4d0d3ffa89d4c5dbce8128b84552c2735f43113
chrisesquerra <esquerra>
2018-09-19 09:50:46 -0500

    Continued exercise completion

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 15 == 0) {
-                System.out.println("fizzbuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("buzz");
-            } else {
-                System.out.println(i);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number between 1 and 10:");
+        int userInt = Integer.parseInt(scanner.nextLine());
+        System.out.println("Would you like to see a table of exponents up to your number?  Yes or No:");
+        String userResponse = scanner.nextLine();
+        if (userResponse.toLowerCase().equals("yes")) {
+            System.out.println("Here is a table of powers!");
+            String head1 = "Number";
+            String head2 = "Squared";
+            String head3 = "Cubed";
+            System.out.format("%10s | %10s | %10s |%n", head1, head2, head3);
+            System.out.println("----------------------------------------");
+            for (int i = 1; i <= userInt; i++) {
+                int square = i * i;
+                int cube = i * i * i;
+                System.out.format("%10d | %10d | %10d |%n", i, square, cube);
             }
+        } else {
+            System.out.println("Sure, that's fine.  Whatever.");
         }
     }
 }
