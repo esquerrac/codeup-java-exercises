91863fb67195594e8709b9a397f115018517acee
chrisesquerra <esquerra>
2018-09-20 15:23:10 -0500

    HighLow Exercise

---
+++ src/HighLow.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class HighLow {
+    public static void main(String[] args) {
+        int randomNumber = Math.floor(Math.random()*100)+1;
+        System.out.println("Ready to guess a random number?");
+        Scanner scanner = new Scanner(System.in);
+        String userResponse = scanner.nextLine();
+        if (userResponse.toLowerCase().contains("y")){
+            while (true){
+                System.out.println("Guess a number between 1 and 100:");
+                int userGuess = scanner.nextInt();
+                if (userGuess==randomNumber){
+                    System.out.println("A WINNER IS YOU");
+                    break;
+                }else if(userGuess>randomNumber){
+                    System.out.println("We don't have the budget for numbers that big!  Guess lower!");
+                    continue;
+                }else if (userGuess<randomNumber){
+                    System.out.println("You can do better than that!  Guess higher!");
+                    continue;
+                }
+            }
+        }
+    }
+}
